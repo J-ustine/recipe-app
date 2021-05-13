921fa24919f0ffc9b1e9d163a9e6e37fb30f3368
J-ustine <78701219+J-ustine@users.noreply.github.com>
2021-05-13 17:04:22 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Hello Wold!
+        <p>
+          In the future, you will find here amazing recipe according to your
+          needs...
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
